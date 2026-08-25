32d45ebd7019089d18d90970e79ebd71d034c33a
kiva01 <kirill.igor.vassiljev@gmail.com>
2026-08-26 00:17:48 +0300

    remove content list

--- 777/src/routes/+page.svelte
+++ 777/src/routes/+page.svelte
@@ -6,12 +6,3 @@
 <h1>Kingshot 777</h1>
 <p>Guides, tier lists, and build orders. Pick a topic from the sidebar or start below.</p>
 
-{#each sections as section (section.title)}
-	<h2>{section.title}</h2>
-	<ul>
-		{#each section.items as item (item.slug)}
-			<li><a href="{base}/guides/{item.slug}">{item.title}</a></li>
-		{/each}
-	</ul>
-{/each}
-
